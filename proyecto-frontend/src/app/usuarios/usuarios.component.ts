d0109e7892e2aac7f2b16cd5d30cd2febf501650
MariaPaulaTovar01 <maria.parga39339@ucaldas.edu.co>
2024-03-02 16:12:47 -0500

    creacion de servicios

--- proyecto-frontend/src/app/usuarios/usuarios.component.ts
+++ proyecto-frontend/src/app/usuarios/usuarios.component.ts
@@ -5,12 +5,17 @@ import { BuscarViajeComponent } from './buscar-viaje/buscar-viaje.component';
 import { ConsultarReservaComponent } from './consultar-reserva/consultar-reserva.component';
 import { ViajesComponent } from '../viajes/viajes.component';
 import { FormularioUsuariosComponent } from '../viajes/formulario-usuarios/formulario-usuarios.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [RouterOutlet,BuscarViajeComponent,
-    ConsultarReservaComponent,ViajesComponent,FormularioUsuariosComponent],
+  imports: [RouterOutlet,
+    BuscarViajeComponent,
+    ConsultarReservaComponent,
+    ViajesComponent,
+    FormularioUsuariosComponent, 
+    CommonModule],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css'
 })
