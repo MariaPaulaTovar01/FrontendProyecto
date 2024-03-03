8b9b072564173747d1c2ab43390fd392e87bd79d
MariaPaulaTovar01 <maria.parga39339@ucaldas.edu.co>
2024-03-02 22:35:53 -0500

    buscar por destino

--- proyecto-frontend/src/app/servicios/usuarios.service.ts
+++ proyecto-frontend/src/app/servicios/usuarios.service.ts
@@ -1,12 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Viaje } from '../entities/viaje';
+import { Cliente } from '../entities/cliente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
 
-  constructor() { }
+  
+  constructor(private httpClient : HttpClient) {
+
+   }
 
 
-  private bdBuscarViaje = "http://localhost:8080/ver/cliente/BuscarDestinos"
+  private bdBuscarViaje = "http://localhost:8080/ver/cliente/BuscarDestino";
+
+
+   BuscarDestino(destino: String): Observable<Viaje[]>{
+    return this.httpClient.post<Viaje[]>(this.bdBuscarViaje, destino);
+   }
+
+  private bdGuardarDatosCliente = "http://localhost:8080/ver/cliente/GuardarInfoCliente"
+
+  GuardarDatosCliente(c :Cliente){
+    return this.httpClient.post(this.bdGuardarDatosCliente, c);
+  }
 }
--- proyecto-frontend/src/app/usuarios/buscar-viaje/buscar-viaje.component.ts
+++ proyecto-frontend/src/app/usuarios/buscar-viaje/buscar-viaje.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ViajesComponent } from '../../viajes/viajes.component';
 import { FormularioUsuariosComponent } from '../../viajes/formulario-usuarios/formulario-usuarios.component';
+import { FormsModule } from '@angular/forms';
+import { UsuariosService } from '../../servicios/usuarios.service';
+import { Viaje } from '../../entities/viaje';
+import { error } from 'console';
+import { CommonModule } from '@angular/common';
+import { Cliente } from '../../entities/cliente';
 
 @Component({
   selector: 'app-buscar-viaje',
   standalone: true,
-  imports: [ViajesComponent,FormularioUsuariosComponent],
+  imports: [ViajesComponent,FormularioUsuariosComponent, FormsModule,ViajesComponent, CommonModule],
   templateUrl: './buscar-viaje.component.html',
   styleUrl: './buscar-viaje.component.css'
 })
-export class BuscarViajeComponent {
+export class BuscarViajeComponent implements OnInit{
+  destino: string;
+  viajes: Viaje[];
+  cliente : Cliente = new Cliente;
+  ngOnInit(): void {
+ 
+    }
+  
+  
+
+  constructor(private usuariosservicio : UsuariosService){
+
+  }
 
   AbrirFomulario(){
     const modal = document.getElementById("buscar_viaje")
@@ -20,5 +38,21 @@ export class BuscarViajeComponent {
     const modal = document.getElementById("buscar_viaje")
     if (modal!= null) 
     modal.style.display="none";
+  }
+  AbrirFomularioUsuario(){
+    const modal = document.getElementById("formulario-clientes")
+    if (modal!= null) 
+    modal.style.display="block";
+  }
+  cerrarFormularioUsuario(){
+    const modal = document.getElementById("formulario-clientes")
+    if (modal!= null) 
+    modal.style.display="none";
+  }
+  
+  mostrarViajes(destino : String ){
+    this.usuariosservicio.BuscarDestino(destino).subscribe(dato =>{
+      this.viajes = dato;
+    });
   }
 }
