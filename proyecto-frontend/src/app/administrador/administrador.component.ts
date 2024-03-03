e00677df30223a6dbeeb0c7092a92d51c5e530b0
DavidOrtiz0 <david.ortiz39271@ucaldas.edu.co>
2024-03-03 18:26:25 -0500

    administrador funcionando

--- proyecto-frontend/src/app/administrador/administrador.component.ts
+++ proyecto-frontend/src/app/administrador/administrador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Administrador } from '../entities/administrador';
@@ -7,6 +7,7 @@ import { ListaPorDiaComponent } from './lista-por-dia/lista-por-dia.component';
 import { ListaPorBusComponent } from './lista-por-bus/lista-por-bus.component';
 import { ViajesComponent } from '../viajes/viajes.component';
 import { BuscarViajeComponent } from '../usuarios/buscar-viaje/buscar-viaje.component';
+import { AdminService } from '../servicios/admin.service';
 
 @Component({
   selector: 'app-administrador',
@@ -22,41 +23,70 @@ import { BuscarViajeComponent } from '../usuarios/buscar-viaje/buscar-viaje.comp
 })
 
 export class AdministradorComponent implements OnInit{
- administrador : Administrador;
+ administrador = new Administrador;
  validacion: boolean;
+ usuario: String;
+ contrasena: String;
 
-constructor() {
-  this.validacion = true;
-  this.administrador = new Administrador;
- }
+constructor(private admService: AdminService) {}
 
   ngOnInit(): void {
-    this.mostrarElementos();
-    this.MostrarAdm();
+    this.mostrarPorDefecto();
   }
 
-  mostrarElementos(){
-    if(this.validacion == true){
-    const modal = document.getElementById("indexAdm")
-      if (modal!= null) 
-      modal.style.display="block";
+  validar(){
+    this.administrador = ({
+      usuario: this.usuario,
+      contrasena: this.contrasena
+    })
 
-      const modal2 = document.getElementById("logIn")
-      if (modal2!= null) 
-      modal2.style.display="none";
-    }
+    this.admService.IniciarSesion(this.administrador).subscribe({
+      next: (DatosUsuario) =>{
+        if(DatosUsuario == true){
+          this.validacionHecha();
+        }else{
+          alert("datos equivocados");
+        }
+        console.log("el dato es: "+ DatosUsuario);
+      },
+      error:(DatosError) =>{
+        console.error(DatosError);
+      },
+      complete: () =>{
+        console.info("accion completada");
+      }
+    });
+    
   }
 
-  MostrarAdm(){
-    if(this.validacion != true){
-      
+  validacionHecha(){
+    if(typeof document != undefined){
+
       const modal = document.getElementById("indexAdm")
-      if (modal!= null) 
-      modal.style.display="none";
+      if(modal!= null){
+        modal.style.display="block";
+      }
 
       const modal2 = document.getElementById("logIn")
-      if (modal2!= null) 
-      modal2.style.display="block";
+      if(modal2!= null){
+        modal2.style.display="none";
+      }
+    }
+
+  }
+
+  mostrarPorDefecto(){
+    if(typeof document != undefined){
+
+      const modal = document.getElementById("indexAdm")
+      if(modal!= null){
+        modal.style.display="none";
+      }
+
+      const modal2 = document.getElementById("logIn")
+      if(modal2!= null){
+        modal2.style.display="block";
+      }
     }
   }
 
